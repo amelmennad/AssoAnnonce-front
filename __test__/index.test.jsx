fd8a3b23413b8e006c340fc5d7fe8be8e1491a4a
amelmennad <amelmnd.dev@gmail.com>
2022-10-18 18:58:02 +0200

    feat (conf): update jest conf to have coverage

--- __test__/index.test.jsx
+++ __test__/index.test.jsx
@@ -2,12 +2,23 @@ import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import Home from "../pages/index";
 
-describe("Home", () => {
-  it("renders a heading", () => {
+describe('Home', () => {
+  it('renders a heading', () => {
     render(<Home />);
 
-    const heading = screen.getByRole("heading", {
-      name: /welcome to next\.js!/i,
+    const heading = screen.getByRole('heading', {
+      name: /Bienvenue sur AssoAnnonce/i,
+    });
+
+    expect(heading).toBeInTheDocument();
+  });
+});
+describe('Home 2', () => {
+  it('renders a heading', () => {
+    render(<Home />);
+
+    const heading = screen.getByRole('heading', {
+      name: /Bienvenue sur AssoAnnonce/i,
     });
 
     expect(heading).toBeInTheDocument();
--- jest.config.js
+++ jest.config.js
@@ -1,25 +1,45 @@
 module.exports = {
-  collectCoverageFrom: ["**/*.{js,jsx,ts,tsx}", "!**/*.d.ts", "!**/node_modules/**"],
+  collectCoverageFrom: [
+    '**/*.{js,jsx,ts,tsx}',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+  ],
   moduleNameMapper: {
     /* Handle CSS imports (with CSS modules)
     https://jestjs.io/docs/webpack#mocking-css-modules */
-    "^.+\\.module\\.(css|sass|scss)$": "identity-obj-proxy",
-    "^@/components/(.*)$": "<rootDir>/components/$1",
+    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+    '^@/components/(.*)$': '<rootDir>/components/$1',
 
     // Handle CSS imports (without CSS modules)
-    "^.+\\.(css|sass|scss)$": "<rootDir>/__mocks__/styleMock.js",
+    '^.+\\.(css|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
 
     /* Handle image imports
     https://jestjs.io/docs/webpack#handling-static-assets */
-    "^.+\\.(jpg|jpeg|png|gif|webp|avif|svg)$": "<rootDir>/__mocks__/fileMock.js",
+    '^.+\\.(jpg|jpeg|png|gif|webp|avif|svg)$':
+      '<rootDir>/__mocks__/fileMock.js',
   },
-  testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/.next/"],
-  testEnvironment: "jsdom",
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
+  coveragePathIgnorePatterns: [
+    '/node_modules',
+    '/.next',
+    '/coverage',
+    '/jest.config.js',
+    '/next.config.js',
+  ],
+  testEnvironment: 'jsdom',
   transform: {
     /* Use babel-jest to transpile tests with the next/babel preset
     https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object */
-    "^.+\\.(js|jsx|ts|tsx)$": ["babel-jest", { presets: ["next/babel"] }],
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
   },
-  transformIgnorePatterns: ["/node_modules/", "^.+\\.module\\.(css|sass|scss)$"],
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  transformIgnorePatterns: [
+    '/node_modules/',
+    '^.+\\.module\\.(css|sass|scss)$',
+  ],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  verbose: true,
 };

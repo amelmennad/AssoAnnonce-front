f286a6b53f64ef6d1ec6d0273d61c0f588b2bda0
amelmennad <amelmnd.dev@gmail.com>
2022-10-22 18:18:22 +0200

    feat (volunteer): update register page

--- pages/benevole/inscription.js
+++ pages/benevole/inscription.js
@@ -1,11 +1,57 @@
 import ButtonCustom from 'component/BootstrapCustom/ButtonCustom';
+import PageContainer from 'component/PageContainer/PageContainer';
 import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
 
 export default function inscription() {
+
+  // console.log('file: inscription.js -> line 11 -> Date.now()', Date.now());
+  // const diff = new Date(Date.now() - new Date(birthday).getTime());
+  // const age = Math.abs(diff.getUTCFullYear() - 1970);
+
+
+  const [lastName, setLastName] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [invalidPassword, setInvalidPassword] = useState(false)
+  const [matchPassword, setMatchPassword] = useState(false)
+  const [birthday, setBirthday] = useState('');
+  const [invalidBirthday, setInvalidBirthday] = useState(false)
+  const [cgu, setCgu] = useState(false);
+  const [invalidCgu, setInvalidCgu] = useState(false)
+
   const [validated, setValidated] = useState(false);
+
+  const handleChangeLastName = (e) => {
+    setLastName(e.target.value)
+  }
+
+  const handleChangeFirstName = (e) => {
+    setFirstName(e.target.value)
+  }
+  const handleChangeEmail = (e) => {
+    setEmail(e.target.value)
+  }
+
+  const checkBirthday = (e) => {
+    const diff = new Date(Date.now() - new Date(e.target.value).getTime());
+    const age = Math.abs(diff.getUTCFullYear() - 1970);
+    console.log('file: inscription.js -> line 32 -> age', age);
+    if (age < 16) {
+      setInvalidBirthday(true)
+    } else if (age >= 16) {
+      setInvalidBirthday(false)
+    }
+    setBirthday(e.target.value)
+  }
+
+
+  const handleChangeCgu = (e) => {
+    setCgu(!cgu)
+  }
 
   const handleSubmit = (event) => {
     const form = event.currentTarget;
@@ -18,65 +64,76 @@ export default function inscription() {
   };
 
   return (
-    <div className='container mt-5'>
-      <div className='card'>
-        <div className='card-header text-center'>Bénévoles</div>
-        <div className='card-body'>
-          <h5 className='card-title text-center'>Inscription</h5>
-
-          <Form noValidate validated={validated} onSubmit={handleSubmit}>
-            <Form.Group className='mb-3 fw-semibold' controlId='lastName'>
-              <Form.Label>Nom</Form.Label>
-              <Form.Control required type='text' />
-            </Form.Group>
-            <Form.Group className='mb-3 fw-semibold' controlId='firstName'>
-              <Form.Label>Prénom</Form.Label>
-              <Form.Control required type='text' placeholder='Prénom' />
-            </Form.Group>
-            <Form.Group className='mb-3 fw-semibold' controlId='birthday'>
-              <Form.Label>Date de naissance</Form.Label>
-              <Form.Control required type='date' />
-              <Form.Text className='text-muted fw-lighter fst-italic'>
-                Pour vous inscrire vous devez avoir 16 ans ou plus
-              </Form.Text>
-            </Form.Group>
-
-            <Form.Group
-              className='mb-3 fw-semibold'
-              controlId='formBasicPassword'
-            >
-              <Form.Label>Password</Form.Label>
-              <Form.Control required type='password' placeholder='Password' />
-              <Form.Text className='text-muted fw-lighter fst-italic'>
-                Votre mot de passe doit comporter entre 8 et 20 caractères,
-                contenir des lettres et des chiffres et ne doit pas contenir
-                d'espaces, de caractères spéciaux ou d'emoji.
-              </Form.Text>
-              <Form.Control.Feedback type='invalid'>
-                Entrer un mot de passe valide
-              </Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group className='mb-3'>
-              <Form.Check
-                required
-                name='terms'
-                label='Agree to terms and conditions'
-                // onChange={handleChange}
-                // isInvalid={!!errors.terms}
-                // feedback={errors.terms}
-                feedbackType='invalid'
-                id='validationFormik0'
-              />
-            </Form.Group>
-            <Button variant='primary' type='submit'>
-              Submit
-            </Button>
-            <ButtonCustom name={'Inscription'} type={'submit'} />
-          </Form>
-        </div>
-        {/* </div> */}
+    <PageContainer>
+      <div className='text-center '>
+        <h4>Inscription</h4>
+        <h6>Bénévoles</h6>
       </div>
-    </div>
+
+      <Form noValidate validated={validated} onSubmit={handleSubmit}>
+        <Form.Group className='mb-3 fw-semibold' controlId='lastName'>
+          <Form.Label>Nom</Form.Label>
+          <Form.Control required type='text' defaultValue={lastName} onChange={handleChangeLastName} />
+        </Form.Group>
+        <Form.Group className='mb-3 fw-semibold' controlId='firstName'>
+          <Form.Label>Prénom</Form.Label>
+          <Form.Control required type='text' placeholder='Prénom' defaultValue={firstName} onChange={handleChangeFirstName} />
+        </Form.Group>
+        <Form.Group className='mb-3 fw-semibold' controlId='birthday' >
+          <Form.Label>Date de naissance</Form.Label>
+          <Form.Control required type='date' defaultValue={birthday} onBlur={checkBirthday} isInvalid={invalidBirthday}
+          />
+          <Form.Text className='text-muted fw-lighter fst-italic'>
+            Pour vous inscrire vous devez avoir 16 ans ou plus
+          </Form.Text>
+          {invalidBirthday && <Form.Control.Feedback type='invalid'>
+            Désolé vous êtes trops jeune 😔
+          </Form.Control.Feedback>}
+        </Form.Group>
+
+        <Form.Group
+          className='mb-3 fw-semibold'
+          controlId='formBasicPassword'
+        >
+          <Form.Label>Mot de passe</Form.Label>
+          <Form.Control required type='password' placeholder='Mot de passe' defaultValue={password} />
+          <Form.Text className='text-muted fw-lighter fst-italic'>
+            Votre mot de passe doit comporter 8 caractères minimum, 1 majuscule, 1 minuscule, 1 chiffre et 1 caractère spécial.
+          </Form.Text>
+          <Form.Control.Feedback type='invalid'>
+            Entrer un mot de passe valide
+          </Form.Control.Feedback>
+        </Form.Group>
+
+        <Form.Group
+          className='mb-3 fw-semibold'
+          controlId='formBasicPassword'
+        >
+          <Form.Label>Confirmation du mot de passe</Form.Label>
+          <Form.Control required type='password' placeholder='Mot de passe' defaultValue={password} />
+          <Form.Control.Feedback type='invalid'>
+            Entrer un mot de passe valide
+          </Form.Control.Feedback>
+        </Form.Group>
+        <Form.Group className='mb-3'>
+          <Form.Check
+            required
+            name='terms'
+            label="J'accepte les conditions d'utilisation"
+            onChange={handleChangeCgu}
+            defaultValue={cgu}
+            isInvalid={invalidCgu}
+            // feedback={errors.terms}
+            feedbackType='invalid'
+            id='validationFormik0'
+          />
+        </Form.Group>
+        <div className='d-flex justify-content-center'>
+          <ButtonCustom className="d-flex justify-content-center" name={'Inscription'} type={'submit'} />
+        </div>
+      </Form>
+
+    </PageContainer>
   );
 }
 /*

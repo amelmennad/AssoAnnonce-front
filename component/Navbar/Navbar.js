e316bec688668dec2eefe178881b04d69eeb4483
amelmennad <amelmnd.dev@gmail.com>
2022-11-05 17:31:03 +0100

    feat (responsive) : end hambuger menu with auto close

--- component/Navbar/Navbar.js
+++ component/Navbar/Navbar.js
@@ -4,8 +4,8 @@ import { AiOutlineClose } from "react-icons/ai";
 import { BiHomeSmile } from "react-icons/bi";
 import { CgProfile } from "react-icons/cg";
 import { GiHamburgerMenu } from "react-icons/gi";
-import { GrAddCircle } from "react-icons/gr";
 import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
 
 function useWindowSize() {
   const isClient = typeof window === "object";
@@ -36,30 +36,65 @@ function useWindowSize() {
 }
 
 export default function NavbarCustom() {
+  const router = useRouter();
   const [visibilityHamburgerMenu, setVisibilityHamburgerMenu] = useState(false);
   const [visibilityMenu, setVisibilityMenu] = useState(false);
+  const [currentPath, setCurrentPath] = useState();
+  const [isMenuOpen, setMenuOpen] = useState(false);
 
   const screenWidth = useWindowSize().width;
 
+  const isVisibleHamburgerMenu = () => {
+    setVisibilityHamburgerMenu(true);
+    setVisibilityMenu(false);
+  };
+
+  const isVisibleMenu = () => {
+    setVisibilityHamburgerMenu(false);
+    setVisibilityMenu(true);
+  };
+
   useEffect(() => {
     if (screenWidth > 961) {
-      setVisibilityHamburgerMenu(false);
-      setVisibilityMenu(true);
+      isVisibleMenu();
     } else {
-      setVisibilityHamburgerMenu(true);
-      setVisibilityMenu(false);
+      isVisibleHamburgerMenu();
     }
   }, [screenWidth]);
 
+  useEffect(() => {
+    if (isMenuOpen) {
+      setMenuOpen(!isMenuOpen);
+    }
+  }, [router.asPath]);
+  // useEffect(() => {
+  //   if (router.asPath != currentPath) {
+  // setCurrentPath(router.asPath);
+  //     setVisibilityHamburgerMenu(true);
+  //     setVisibilityMenu(false); //     isVisibleHamburgerMenu();
+  //     //     console.log("file: Navbar.js -> line 70 -> newCurrentPath", newCurrentPath);
+  //   }
+
+  //   console.log("file: Navbar.js -> line 43 -> currentPath", currentPath);
+  //   console.log("file: Navbar.js -> line 43 -> router.asPath", router.asPath);
+  //   // console.log("file: Navbar.js -> line 68 -> newCurrentPath", newCurrentPath);
+  //   //   if (router.asPath != currentPath) {
+  //   //     isVisibleHamburgerMenu();
+
+  //   //     console.log("file: Navbar.js -> line 70 -> newCurrentPath", newCurrentPath);
+  //   //     // setCurrentPath(newCurrentPath);
+  //   //   }
+  // }, [router.asPath]);
+
   return (
     <nav className={styles.navbar + " sticky-top"}>
-      {visibilityHamburgerMenu ? (
+      {!isMenuOpen ? (
         <div className={styles.divHamburgerMenu}>
           <div
             className={styles.divHamburgerMenu}
             onClick={(e) => {
-              setVisibilityHamburgerMenu(false);
-              setVisibilityMenu(true);
+              isVisibleMenu();
+              setMenuOpen(true);
             }}
           >
             <GiHamburgerMenu size={22} value={{ className: styles.reactIcons }} />
@@ -67,12 +102,13 @@ export default function NavbarCustom() {
         </div>
       ) : (
         <>
-          {screenWidth < 961 && visibilityMenu && (
+          {screenWidth < 961 && isMenuOpen && (
             <div
               className={styles.divCloseMenu}
               onClick={(e) => {
                 setVisibilityHamburgerMenu(true);
                 setVisibilityMenu(false);
+                setMenuOpen(false);
               }}
             >
               <AiOutlineClose size={18} value={{ className: styles.reactIcons }} />
